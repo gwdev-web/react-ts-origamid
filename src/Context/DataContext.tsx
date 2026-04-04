4bb33c76143ef52b43a0b03b0f840910c9072cc9
gwdev-web <gwmb.contato@gmail.com>
2026-04-04 10:10:21 -0300

    feat: data reativa

--- src/Context/DataContext.tsx
+++ src/Context/DataContext.tsx
@@ -5,6 +5,10 @@ interface IDataContext {
   data: IVenda[] | null;
   loading: boolean;
   error: string | null;
+  inicio: string;
+  setInicio: React.Dispatch<React.SetStateAction<string>>;
+  final: string;
+  setFinal: React.Dispatch<React.SetStateAction<string>>;
 }
 
 interface IVenda {
@@ -28,13 +32,28 @@ export const useData = () => {
   return context;
 };
 
+function getDate(n: number) {
+  const date = new Date();
+  date.setDate(date.getDate() - n);
+  const dd = String(date.getDate()).padStart(2, "0");
+  const mm = String(date.getMonth() + 1).padStart(2, "0");
+  const yyyy = date.getFullYear();
+
+  return `${yyyy}-${mm}-${dd}`;
+}
+
 export const DataContextProvider = ({ children }: React.PropsWithChildren) => {
+  const [inicio, setInicio] = React.useState(getDate(14));
+  const [final, setFinal] = React.useState(getDate(0));
+
   const { data, loading, error } = useFetch<IVenda[]>(
-    "https://data.origamid.dev/vendas/",
+    `https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`,
   );
 
   return (
-    <DataContext.Provider value={{ data, loading, error }}>
+    <DataContext.Provider
+      value={{ data, loading, error, inicio, setInicio, final, setFinal }}
+    >
       {children}
     </DataContext.Provider>
   );
